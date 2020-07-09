7d39705c78b8edaa4595ed74cb3413c80c80e893
Ch3shireDev <the.smiling.cat.of.cheshire@gmail.com>
2020-07-09 21:06:37 +0200

    Update to Angular 10

---
+++ ClientApp/src/app/multiple-choice/multiple-choice.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MultipleChoiceComponent } from './multiple-choice.component';
+
+
+
+@NgModule({
+  declarations: [MultipleChoiceComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MultipleChoiceModule { }

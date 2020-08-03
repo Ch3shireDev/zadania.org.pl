51ad0477c548406687530f315cd3a2e01f498986
Ch3shireDev <the.smiling.cat.of.cheshire@gmail.com>
2020-08-03 09:13:33 +0200

    Add Points and UserVote to ProblemView

--- ResourceAPI/ProblemLibrary/ProblemView.cs
+++ ResourceAPI/ProblemLibrary/ProblemView.cs
@@ -8,7 +8,8 @@ namespace ProblemLibrary
         public string Name { get; set; }
         public string Content { get; set; }
         public bool IsSolved { get; set; }
-
+        public int Points { get; set; }
+        public int UserVote { get; set; }
         public IEnumerable<AnswerView> Answers { get; set; }
     }
 }
